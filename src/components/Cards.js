4c97dd9641db339188f148cbc286364f4cf8e3b5
Will Adams <bushblade@gmail.com>
2018-11-14 23:10:12 +0000

    spring in component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import { Transition, animated } from 'react-spring'
 
 import Card from './Card'
 import AddCard from './AddCard'
 
 const Cards = ({ store }) => {
   return (
-    <>
-      {store.cards.map(card => (
-        <Card card={card} key={card.cardId} store={store} />
-      ))}
+    <Fragment>
+      <Transition
+        native
+        keys={item => item.cardId}
+        items={store.cards}
+        from={{ opacity: 0 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}
+        trail={100}>
+        {item => props => (
+          <animated.div className="column" style={props}>
+            <Card card={item} store={store} />
+          </animated.div>
+        )}
+      </Transition>
       <AddCard store={store} />
-    </>
+    </Fragment>
   )
 }
 
