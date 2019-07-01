2c7435f1ffb5c53dce6cd28337b1767e0ce80b0b
Will Adams <bushblade@gmail.com>
2019-07-01 19:00:45 +0100

    Only allow one color picker to be open

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,30 +1,31 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import { Context } from '../context'
 import { colors } from '../defaultCards'
 
 export default ({ card }) => {
   const { changeColor } = useContext(Context)
   const [showMenu, setMenu] = useState(false)
+  const menuRef = useRef()
 
   const toggleMenu = () => setMenu(!showMenu)
 
-  const closeDropdown = e => {
-    if (!e.target.classList.contains('color-pick')) {
-      setMenu(false)
-      document.removeEventListener('click', closeDropdown)
-    }
-  }
-
   useEffect(() => {
+    const closeDropdown = e => {
+      if (e.target !== menuRef.current) {
+        setMenu(false)
+      }
+    }
     if (showMenu) {
       document.addEventListener('click', closeDropdown)
     }
-  }, [showMenu])
+    return () => document.removeEventListener('click', closeDropdown)
+  }, [showMenu, menuRef])
 
   return (
     <span
       className={`icon color-pick has-tooltip ${showMenu ? 'active' : ''}`}
       onClick={toggleMenu}
+      ref={menuRef}
     >
       <i className="fas fa-chevron-down" />
       <div className="color-picker" style={{ backgroundColor: card.color }}>
